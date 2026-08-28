21053c20cea67336be5ac8e2bbcfd0c2fb8ed83a
ermi9s <ermiayele1122334477@gmail.com>
2026-08-29 02:04:24 +0300

    Refactor directory scanning logic and improve environment file detection

--- internal/env/scanner.go
+++ internal/env/scanner.go
@@ -1,41 +1,70 @@
 package env
 
 import (
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+
+var skipDirs = map[string]bool{
+	".git":         true,
+	".hg":          true,
+	".svn":         true,
+	"node_modules": true,
+	"vendor":       true,
+	"target":       true,
+	"dist":         true,
+	"build":        true,
+	".next":        true,
+	".nuxt":        true,
+	".turbo":       true,
+	".cache":       true,
+	".terraform":   true,
+	".idea":        true,
+	".vscode":      true,
+	"__pycache__":  true,
+	".venv":        true,
+	"venv":         true,
+	"env":          true, 
+	".mypy_cache":  true,
+	".pytest_cache": true,
+	".tox":         true,
+}
+
+
 func Scan(root string) ([]string, error) {
 	var files []string
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
+			if os.IsPermission(err) {
+				if d != nil && d.IsDir() {
+					return filepath.SkipDir
+				}
+				return nil
+			}
 			return err
 		}
 
-		if info.IsDir() {
-			if shouldSkipDirectory(path, root) {
+		if d.IsDir() {
+			if path != root && skipDirs[d.Name()] {
 				return filepath.SkipDir
 			}
-
 			return nil
 		}
 
-		name := info.Name()
-
-		if strings.HasPrefix(name, ".env") {
+		if isEnvFile(d.Name()) {
 			relative, err := filepath.Rel(root, path)
 			if err != nil {
 				return err
 			}
-
 			files = append(files, relative)
 		}
 
 		return nil
 	})
-
 	if err != nil {
 		return nil, err
 	}
@@ -43,22 +72,10 @@ func Scan(root string) ([]string, error) {
 	return files, nil
 }
 
-func shouldSkipDirectory(path, root string) bool {
-	relative, err := filepath.Rel(root, path)
-	if err != nil {
-		return false
-	}
 
-	if relative == "." {
-		return false
-	}
-
-	first := strings.Split(relative, string(os.PathSeparator))[0]
-
-	switch first {
-	case ".git", "node_modules", "vendor", "target", "dist", "build":
+func isEnvFile(name string) bool {
+	if name == ".env" {
 		return true
 	}
-
-	return false
+	return strings.HasPrefix(name, ".env.")
 }
